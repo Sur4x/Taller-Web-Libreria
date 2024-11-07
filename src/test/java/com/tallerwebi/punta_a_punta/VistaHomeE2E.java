ea57d7cab2d2adbff1e2575d07337322a6699d39
Jeronimo P <jeronimo1080@hotmail.com>
2024-11-07 18:27:12 -0300

    Se agrega test punta a punta de la vista home.

---
+++ src/test/java/com/tallerwebi/punta_a_punta/VistaHomeE2E.java
@@ -0,0 +1,85 @@
+package com.tallerwebi.punta_a_punta;
+
+import com.microsoft.playwright.*;
+import com.tallerwebi.punta_a_punta.vistas.VistaHome;
+import com.tallerwebi.punta_a_punta.vistas.VistaLogin;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+
+public class VistaHomeE2E {
+
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    VistaHome vistaHome;
+    VistaLogin vistaLogin;
+
+    @BeforeAll
+    static void abrirNavegador() {
+        playwright = Playwright.create();
+        //browser = playwright.chromium().launch();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(700));
+    }
+
+    @AfterAll
+    static void cerrarNavegador() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void crearContextoYPagina() {
+        context = browser.newContext();
+        Page page = context.newPage();
+        vistaLogin = new VistaLogin(page);
+        vistaLogin.escribirEMAIL("asd@asd");
+        vistaLogin.escribirClave("asd");
+        vistaLogin.darClickEnIniciarSesion();
+        vistaHome = new VistaHome(page);
+    }
+
+    @AfterEach
+    void cerrarContexto() {
+        context.close();
+    }
+
+    @Test
+    void cuandoElUsuarioHaceClickEnElBotonDelLogoLoDirigeALaVistaHome() {
+        vistaHome.darClickEnElLogo();
+
+        String url = vistaHome.obtenerURLActual();
+        assertThat(url, containsStringIgnoringCase("/club/home"));
+    }
+
+    @Test
+    void cuandoElUsuarioHaceClickEnElBotonCrearNuevoClubLoDirigeALaVistaCrearNuevoClub() {
+        vistaHome.darClickEnCrearNuevoClub();
+        String url = vistaHome.obtenerURLActual();
+        assertThat(url, containsStringIgnoringCase("/crearClub"));
+    }
+
+    @Test
+    void cuandoElUsuarioBuscaUnClubConUnNombreEspecificoCambiaLaUrl() {
+        vistaHome.escribirEnElBuscador("asd");
+        vistaHome.hacerClickEnElBotonBuscar();
+        String url = vistaHome.obtenerURLActual();
+        assertThat(url, containsStringIgnoringCase("/club/buscar?query=asd"));
+    }
+
+    @Test
+    void enElHomeDebeHaberUnSubtituloConClubsDeTerror() {
+        String texto = vistaHome.obtenerTextoClubsDeTerror();
+        assertThat("Explora nuestros clubs de Terror", equalToIgnoringCase(texto));
+    }
+
+    @Test
+    void enElHomeDebeHaberUnClubQueAlHacerClickMeRedireccioneAVistaClub() {
+        vistaHome.hacerClickEnUnClub27();
+        String url = vistaHome.obtenerURLActual();
+        String clubId = url.replaceAll(".*/club/(\\d+)","$1");
+        assertThat(url, containsStringIgnoringCase("/club/" + clubId));
+    }
+
+}
---
+++ src/test/java/com/tallerwebi/punta_a_punta/vistas/VistaHome.java
@@ -0,0 +1,39 @@
+package com.tallerwebi.punta_a_punta.vistas;
+
+import com.microsoft.playwright.Page;
+
+import java.io.File;
+
+public class VistaHome extends VistaWeb{
+
+    public VistaHome(Page page) {
+        super(page);
+        page.navigate("localhost:8080/club/home");
+
+    }
+
+    public void darClickEnCrearNuevoClub(){
+        this.darClickEnElElemento("div a.btn.btn-primary.me-2");
+    }
+
+    public void darClickEnElLogo(){
+        this.darClickEnElElemento("nav div a.navbar-brand");
+    }
+
+    public void escribirEnElBuscador(String buscar){
+        escribirEnElElemento("form input.form-control.me-2" ,buscar);
+    }
+
+    public void hacerClickEnElBotonBuscar(){
+        this.darClickEnElElemento("form button.btn.btn-outline-light");
+    }
+
+    public String obtenerTextoClubsDeTerror(){
+        return this.obtenerTextoDelElemento("#clubTerror");
+    }
+
+    public void hacerClickEnUnClub27(){
+        this.darClickEnElElemento("#club-27");
+    }
+
+}
